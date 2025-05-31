bfcb027ff61b52c11704d5b6c4b4354269306474
Rashmi Mendis <rashmimendis20@gmail.com>
2025-05-31 21:55:27 +0530

    Project Initialization

---
+++ Employee_Management_System/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  
+
+  return (
+    <>App</>
+      
+  )
+}
+
+export default App
---
+++ Employee_Management_System/src/Components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+
+const Login = () => {
+  return (
+    <div>
+        <div>
+            <h2>Login Page</h2>
+            <form>
+                <div>
+                    <label htmlFor="email"><strong>Email:</strong></label>
+                    <input type='email' name='email' autoComplete='off' placeholder='Enter Email'
+                    className='form-control rounded-0'/>
+                </div>
+                <div>
+                    <label htmlFor="password"><strong>Password:</strong></label>
+                    <input type='password' name='email' autoComplete='off' placeholder='Enter Password'
+                    className='form-control rounded-0'/>
+                </div>
+            </form>
+        </div>
+    </div>
+  )
+}
+export default Login
